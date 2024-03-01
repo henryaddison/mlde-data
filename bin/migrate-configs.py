0314ab6b71283516c73da61571d6eb6d2e2e7e2e
Henry Addison <henryaddison@users.noreply.github.com>
2024-03-01 15:34:09 +0000

    helper script to update dataset config files

---
+++ bin/migrate-configs.py
@@ -0,0 +1,35 @@
+# TO DO (after GH merge): python migrate-configs $DERIVED_DATA/moose/nc-datasets/*/ds-config.yml
+# DONE: python migrate-configs src/mlde_data/configs/datasets/*.yml
+
+import sys
+import yaml
+
+for fpath in sys.argv[1:]:
+    print(fpath, flush=True)
+    with open(fpath, "r") as f:
+        config = yaml.safe_load(f)
+
+    orig_predictand = config.pop("predictand")
+
+    new_predictands = {
+        "variables": [orig_predictand["variable"]],
+        "resolution": orig_predictand["resolution"],
+    }
+
+    config["predictands"] = new_predictands
+
+    orig_predictors = config.pop("predictors")
+    orig_resolution = config.pop("resolution")
+
+    new_predictors = {
+        "variables": [v["variable"] for v in orig_predictors],
+        "resolution": orig_resolution,
+    }
+
+    config["predictors"] = new_predictors
+
+    if "split" in config:
+        config["split"] = config.pop("split")
+
+    with open(fpath, "w") as f:
+        yaml.dump(config, f, default_flow_style=False)
