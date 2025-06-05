c379cf5f15a842909f590cef48e35362516da84a
Henry Addison <henryaddison@users.noreply.github.com>
2025-06-05 13:18:22 +0100

    make sure to load dotenv in etl entry script

--- bin/moose/etl-variable.py
+++ bin/moose/etl-variable.py
@@ -1,13 +1,15 @@
+from dotenv import load_dotenv
 import logging
 from pathlib import Path
+import typer
 from typing import List
 import yaml
-
-import typer
 
 from mlde_data.bin.options import DomainOption, CollectionOption
 from mlde_data.bin.moose import extract, convert, clean
 from mlde_data.bin.variable import create as create_variable
+
+load_dotenv()  # take environment variables from .env
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(levelname)s %(asctime)s: %(message)s")
