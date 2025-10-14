2305a6db67e58ccf5b9ceb86f85f3e3314df0307
Henry Addison <henryaddison@users.noreply.github.com>
2025-10-14 13:57:56 +0100

    sample helper just takes in and outputs one file
    move logic about handling multiple files elsewhere

--- src/mlde_data/bin/__init__.py
+++ src/mlde_data/bin/__init__.py
@@ -4,7 +4,6 @@ import numpy as np
 import os
 from pathlib import Path
 import typer
-from typing import List
 import xarray as xr
 
 from . import dataset, etl, moose, variable
@@ -24,38 +23,29 @@ app.add_typer(variable.app, name="variable")
 
 
 @app.command()
-def sample(files: List[Path], output_dir: Path = None, dim: str = "time"):
-    for file in files:
-        ds = xr.open_dataset(file)
+def sample(file: Path, output_file: Path, dim: str = "time"):
+    ds = xr.open_dataset(file)
 
-        # take something from first day of each mont
-        doy_mask = ds[f"{dim}.dayofyear"] % 30 == 0
-        sampled_ds = ds.sel({dim: doy_mask}).load()
+    # take something from first day of each mont
+    doy_mask = ds[f"{dim}.dayofyear"] % 30 == 0
+    sampled_ds = ds.sel({dim: doy_mask}).load()
 
-        # if covers a long time period, take only specific years
-        year_mask = (ds["time.year"] + (ds["time.month"] == 12)).isin([1981,2000,2021,2040,2061,2080])
-        if np.any(year_mask):
-            sampled_ds = sampled_ds.sel({dim: year_mask})
+    # if covers a long time period, take only specific years
+    year_mask = (ds["time.year"] + (ds["time.month"] == 12)).isin([1981,2000,2021,2040,2061,2080])
+    if np.any(year_mask):
+        sampled_ds = sampled_ds.sel({dim: year_mask})
 
-        # if ensemble member dimension exists, take only first 3 members
-        if "ensemble_member" in sampled_ds.dims and len(sampled_ds["ensemble_member"]) >= 3:
-            sampled_ds = sampled_ds.isel(ensemble_member=[0,1,2])
+    # if ensemble member dimension exists, take only first 3 members
+    if "ensemble_member" in sampled_ds.dims and len(sampled_ds["ensemble_member"]) >= 3:
+        sampled_ds = sampled_ds.isel(ensemble_member=[0,1,2])
+    ds.close()
 
-        sampled_ds = sampled_ds
+    # ensure output directory exists
+    os.makedirs(output_file.parent, exist_ok=True)
+    output_file = str(output_file)
 
-        ds.close()
-        del ds
-
-        if output_dir is not None:
-            output_file = Path(output_dir) / Path(file).relative_to(Path(file).anchor)
-            os.makedirs(output_file.parent, exist_ok=True)
-            output_file = str(output_file)
-        else:
-            output_file = file
-        print(f"Saving {output_file}")
-
-        sampled_ds.to_netcdf(output_file)
-        del sampled_ds
+    logger.info(f"Saving {output_file}")
+    sampled_ds.to_netcdf(output_file)
 
 
 if __name__ == "__main__":
