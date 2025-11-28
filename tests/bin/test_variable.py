2a7c2df5c1bcc66e6656e9c5dcde1ad6d1e0bee5
Henry Addison <henryaddison@users.noreply.github.com>
2025-11-28 10:40:15 +0000

    add test for creating 8.8km engwales target pr

--- tests/bin/test_variable.py
+++ tests/bin/test_variable.py
@@ -139,3 +139,69 @@ def test_create_target(tmp_path):
     ).filepath(year)
 
     xr.open_dataset(output_filepath)  # will raise error if file is invalid
+
+
+def test_create_4x_engwales_target(tmp_path):
+    input_base_dir = Path(
+        os.path.dirname(__file__),
+        "..",
+        "fixtures",
+        "files",
+        "variables",
+        "raw",
+        "moose",
+    )
+    output_base_dir = tmp_path
+
+    collection = "land-cpm"
+    frequency = "1hr"
+    config_path = files("mlde_data").joinpath(
+        f"../../config/variables/{frequency}/{collection}/targets/pr.yml"
+    )
+    year = 1981
+    ensemble_member = "01"
+    domain = "engwales"
+    scenario = "rcp85"
+    scale_factor = 4
+    target_resolution = "2.2km-coarsened-4x"
+
+    result = runner.invoke(
+        app,
+        [
+            "variable",
+            "create",
+            "--config-path",
+            str(config_path),
+            "--scenario",
+            scenario,
+            "--ensemble-member",
+            ensemble_member,
+            "--year",
+            str(year),
+            "--domain",
+            domain,
+            "--scale-factor",
+            scale_factor,
+            "--target-resolution",
+            target_resolution,
+            "--input-base-dir",
+            str(input_base_dir),
+            "--output-base-dir",
+            str(output_base_dir),
+            "--no-validate",
+        ],
+    )
+    assert result.exit_code == 0
+
+    output_filepath = VariableMetadata(
+        base_dir=output_base_dir,
+        collection=collection,
+        scenario=scenario,
+        ensemble_member=ensemble_member,
+        variable="pr",
+        frequency=frequency,
+        resolution=target_resolution,
+        domain=domain,
+    ).filepath(year)
+
+    xr.open_dataset(output_filepath)  # will raise error if file is invalid
