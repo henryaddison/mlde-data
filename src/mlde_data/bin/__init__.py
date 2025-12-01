0cb46c47b7a5d7816cfcc1675df35c6ac539251f
Henry Addison <henryaddison@users.noreply.github.com>
2025-12-01 11:52:25 +0000

    don't let sample blow up if no time dimension
    it's probably a small static file anyway

--- src/mlde_data/bin/__init__.py
+++ src/mlde_data/bin/__init__.py
@@ -26,20 +26,27 @@ app.add_typer(variable.app, name="variable")
 def sample(file: Path, output_file: Path, dim: str = "time"):
     ds = xr.open_dataset(file)
 
-    # take something from first day of each month
-    doy_mask = ds[f"{dim}.is_month_start"]
-    sampled_ds = ds.sel({dim: doy_mask}).load()
+    if dim in ds.dims:
+        # take something from first day of each month
+        doy_mask = ds[f"{dim}.is_month_start"]
+        sampled_ds = ds.sel({dim: doy_mask}).load()
 
-    # if covers a long time period, take two years in 10
-    year_mask = (
-        (sampled_ds["time.year"] + (sampled_ds["time.month"] == 12)) % 10
-    ).isin([0, 5])
-    if np.any(year_mask):
-        sampled_ds = sampled_ds.sel({dim: year_mask})
+        # if covers a long time period, take two years in 10
+        year_mask = (
+            (sampled_ds["time.year"] + (sampled_ds["time.month"] == 12)) % 10
+        ).isin([0, 5])
+        if np.any(year_mask):
+            sampled_ds = sampled_ds.sel({dim: year_mask})
+    else:
+        logger.warning(
+            f"Dimension {dim} not found in dataset dimensions {ds.dims}. No {dim} sampling applied."
+        )
+        sampled_ds = ds.load()
 
     # if ensemble member dimension exists, take only first 3 members
     if "ensemble_member" in sampled_ds.dims and len(sampled_ds["ensemble_member"]) >= 3:
         sampled_ds = sampled_ds.isel(ensemble_member=[0, 1, 2])
+
     ds.close()
 
     # ensure output directory exists
