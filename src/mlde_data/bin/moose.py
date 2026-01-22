3cce27dc1e3aaa71e6e68e1c3cc8a2f608844ca8
Henry Addison <henryaddison@users.noreply.github.com>
2026-01-22 12:11:02 +0000

    handle multiple cubes in extracting and converting moose data

--- src/mlde_data/bin/moose.py
+++ src/mlde_data/bin/moose.py
@@ -75,7 +75,7 @@ class MoosePPVariableMetadata(VariableMetadata):
         return os.path.join(self.ppdata_dirpath(year), "*.pp")
 
 
-def _load_cube(pp_files, variable, collection):
+def _load_cubes(pp_files, variable, collection):
     if variable == "pr" and collection == CollectionOption.gcm:
         # for some reason precip extract for GCM has a mean and max hourly cell method version
         # only want the mean version
@@ -84,7 +84,7 @@ def _load_cube(pp_files, variable, collection):
         )
     else:
         constraint = None
-    return iris.load_cube(pp_files, constraint=constraint)
+    return iris.load(pp_files, constraints=constraint)
 
 
 def _domain_and_resolution_from_collection(collection: CollectionOption):
@@ -221,8 +221,9 @@ def extract(
     output.check_returncode()
 
     # make sure have the correct amount of data from moose
-    cube = _load_cube(str(os.path.join(pp_dirpath, "*.pp")), variable, collection)
-    assert cube.coord("time").shape[0] == FREQ2TIMELEN[frequency]
+    cubes = _load_cubes(str(os.path.join(pp_dirpath, "*.pp")), variable, collection)
+    for cube in cubes:
+        assert cube.coord("time").shape[0] == FREQ2TIMELEN[frequency]
 
 
 @app.command()
@@ -271,19 +272,20 @@ def convert(
     )
     output_filepath = output_var_meta.filepath(year)
 
-    src_cube = _load_cube(
+    src_cubes = _load_cubes(
         str(input_moose_pp_varmeta.pp_files_glob(year)), variable, collection
     )
 
     # bug in some data means the final grid_latitude bound is very large (1.0737418e+09)
-    if collection == CollectionOption.cpm and any(
-        [variable.startswith(var) for var in ["xwind", "ywind", "spechum", "temp"]]
-    ):
-        bounds = np.copy(src_cube.coord("grid_latitude").bounds)
-        # make sure it really is much larger than expected (in case this gets fixed)
-        assert bounds[-1][1] > 8.97
-        bounds[-1][1] = 8.962849
-        src_cube.coord("grid_latitude").bounds = bounds
+    for src_cube in src_cubes:
+        if collection == CollectionOption.cpm and any(
+            [variable.startswith(var) for var in ["xwind", "ywind", "spechum", "temp"]]
+        ):
+            bounds = np.copy(src_cube.coord("grid_latitude").bounds)
+            # make sure it really is much larger than expected (in case this gets fixed)
+            assert bounds[-1][1] > 8.97
+            bounds[-1][1] = 8.962849
+            src_cube.coord("grid_latitude").bounds = bounds
 
     typer.echo(f"Saving to {output_filepath}...")
     # ensure target directory exists
@@ -299,7 +301,7 @@ def convert(
             tmp_path = tmpf.name
 
         # Save to the temporary file
-        iris.save(src_cube, tmp_path)
+        iris.save(src_cubes, tmp_path)
 
         # Move the completed file into the final location. Use shutil.move
         # to handle cross-filesystem renames (e.g., local /tmp -> NFS).
